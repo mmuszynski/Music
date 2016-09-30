14dbf76ade0f64f61f180561d8162f6c366efe4a
Mike Muszynski <mmuszynski@gmail.com>
2016-09-30 13:16:04 -0400

    Initial Commit

---
+++ MusicStaffView/MusicStaffView.h
@@ -0,0 +1,19 @@
+//
+//  MusicStaffView.h
+//  MusicStaffView
+//
+//  Created by Mike Muszynski on 9/30/16.
+//  Copyright © 2016 Mike Muszynski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MusicStaffView.
+FOUNDATION_EXPORT double MusicStaffViewVersionNumber;
+
+//! Project version string for MusicStaffView.
+FOUNDATION_EXPORT const unsigned char MusicStaffViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MusicStaffView/PublicHeader.h>
+
+
