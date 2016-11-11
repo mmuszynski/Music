b3de44f946b043b5481ee8c6be184d181c56f6e5
Mike Muszynski <mmuszynski@gmail.com>
2016-11-11 11:50:33 -0500

    changed access level in musicstaffviewnote

--- MusicStaffView/MusicStaffViewNote.swift
+++ MusicStaffView/MusicStaffViewNote.swift
@@ -14,10 +14,10 @@ public func +(lhs: MusicStaffViewNote, rhs: Int) -> MusicStaffViewNote {
 
 public class MusicStaffViewNote: MusicStaffViewElement {
     
-    var name : MusicStaffViewNoteName = .c
-    var length : MusicStaffViewNoteLength = .quarter
-    var octave : Int = 4
-    var accidental : MusicStaffViewAccidentalType = .none
+    public var name : MusicStaffViewNoteName = .c
+    public var length : MusicStaffViewNoteLength = .quarter
+    public var octave : Int = 4
+    public var accidental : MusicStaffViewAccidentalType = .none
     
     public required init(name: MusicStaffViewNoteName, accidental: MusicStaffViewAccidentalType, length: MusicStaffViewNoteLength, octave: Int) {
         self.name = name
