8527db194bccba6ab1072ac4e6360a9411199de6
Mike Muszynski <mmuszynski@gmail.com>
2016-12-20 10:30:21 -0500

    added readme

---
+++ MusicStaffView/MusicInterval.swift
@@ -0,0 +1,9 @@
+//
+//  MusicInterval.swift
+//  MusicStaffView
+//
+//  Created by Mike Muszynski on 12/20/16.
+//  Copyright © 2016 Mike Muszynski. All rights reserved.
+//
+
+import Foundation
--- MusicStaffViewTests/MusicStaffViewTests.swift
+++ MusicStaffViewTests/MusicStaffViewTests.swift
@@ -21,16 +21,16 @@ class MusicStaffViewTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testNote() {
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMajorScales() {
+        
+    }
+    
+    func testMinorScales() {
+        
     }
     
 }
