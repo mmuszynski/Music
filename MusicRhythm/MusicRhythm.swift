96b8e1c7fb772b77f612acd19d77619f97ff4ab3
Mike Muszynski <mmuszynski@gmail.com>
2018-01-04 20:36:42 -0500

    cherry picked from 1.5

--- MusicRhythm/MusicRhythm.swift
+++ MusicRhythm/MusicRhythm.swift
@@ -12,9 +12,10 @@ public enum MusicRhythm {
     case breve, whole, half, quarter, eighth, sixteenth, thirtysecond, sixtyfourth
     case semibreve, minim, crotchet, quaver, semiquaver, demisemiquaver, hemidemisemiquaver
     indirect case dotted(length: MusicRhythm)
+    indirect case compound(number: Int, duration: MusicRhythm)
     
     /// Duration of the note relative to the sixty-fourth note or hemidemisemiquaver.
-    var duration: Int {
+    private var baseDuration: Int {
         switch self {
         case .breve:
             return 128
@@ -32,17 +33,33 @@ public enum MusicRhythm {
             return 2
         case .sixtyfourth, .hemidemisemiquaver:
             return 1
+        case .compound(_):
+            fatalError("Compound Rhythm has no base duration")
         default:
-            return duration(of: self, withDots: 0)
+            return baseDuration(of: self, withDots: 0)
         }
     }
     
-    private func duration(of base: MusicRhythm, withDots dots: Int) -> Int {
+    public var duration: Double {
+        switch self {
+        case .compound(let number, let base):
+            let duration = base.duration
+            return duration / Double(number)
+        default:
+            return Double(self.baseDuration)
+        }
+    }
+    
+    public func duration(relativeTo otherRhythm: MusicRhythm) -> Double {
+        return self.duration / otherRhythm.duration
+    }
+    
+    private func baseDuration(of base: MusicRhythm, withDots dots: Int) -> Int {
         if case .dotted(let inner) = base {
-            return self.duration(of: inner, withDots: dots + 1)
+            return self.baseDuration(of: inner, withDots: dots + 1)
         } else {
             let power = Int(pow(2.0, Double(dots)))
-            return 2 * base.duration - base.duration / power
+            return 2 * base.baseDuration - base.baseDuration / power
         }
     }
 }
@@ -59,6 +76,48 @@ extension MusicRhythm: Hashable {
     /// Hash values are not guaranteed to be equal across different executions of
     /// your program. Do not save hash values to use during a future execution.
     public var hashValue: Int {
-        return self.duration
+        return self.duration.hashValue
     }
 }
+
+extension MusicRhythm: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .breve:
+            return "Breve"
+        case .whole:
+            return "Whole"
+        case .half:
+            return "Half"
+        case .quarter:
+            return "Quarter"
+        case .eighth:
+            return "Eighth"
+        case .sixteenth:
+            return "Sixteenth"
+        case .thirtysecond:
+            return "Thirty-second"
+        case .sixtyfourth:
+            return "Sixty-fourth"
+        case .semibreve:
+            return "Semibreve"
+        case .minim:
+            return "Minim"
+        case .crotchet:
+            return "Crotchet"
+        case .quaver:
+            return "Quaver"
+        case .semiquaver:
+            return "Semiquaver"
+        case .demisemiquaver:
+            return "Demisemiquaver"
+        case .hemidemisemiquaver:
+            return "Hemidemisemiquaver"
+        case .dotted(let length):
+            return "Dotted" + length.description
+        case .compound(let number, let duration):
+            return "Compound: " + "\(number)" + " in " + duration.description
+        }
+    }
+}
+
