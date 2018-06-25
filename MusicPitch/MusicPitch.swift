8ab87968d90f8cfcf621c50d2b7cff430822c91f
Mike Muszynski <mmuszynski@gmail.com>
2018-06-25 17:22:16 -0400

    added description info?

--- MusicPitch/MusicPitch.swift
+++ MusicPitch/MusicPitch.swift
@@ -13,7 +13,7 @@ import Foundation
 ///While frequency is the fundamental quality that determines what a pitch will sound like, MusicPitch describes the discrete units used in Western music used to name and reproduce notes of certain frequencies. In Western music, there are generally assumed to be twelve discrete, named semitones which comprise an interval span called an octave.
 ///
 ///In many cases, a pitch can be described by more than one name. In this case, these pitches are considered 'enharmonically equivalent' (although their frequencies will be different, for more, see `frequency(with referencePitch: at frequency:)`).
-public struct MusicPitch: Codable, CustomStringConvertible {
+public struct MusicPitch: Codable, CustomStringConvertible {    
     public var description: String {
         var name = self.name.description
         name += accidental.description
