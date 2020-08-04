aa4df62a73998f2a83de4d9717395871e12f1f6d
Mike Muszynski <mmuszynski@gmail.com>
2020-08-04 09:33:54 -0400

    modified to make cross platform

--- Music/Music.h
+++ Music/Music.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Mike Muszynski. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Music.
 FOUNDATION_EXPORT double MusicVersionNumber;
