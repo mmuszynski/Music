b4893a9ef49d1dc6e3b453414676f861d4343306
Mike Muszynski <mmuszynski@gmail.com>
2023-06-19 09:17:01 -0400

    publicize important changes, duh

--- Sources/Music/MusicNote/MusicNote+Modifiers.swift
+++ Sources/Music/MusicNote/MusicNote+Modifiers.swift
@@ -12,27 +12,27 @@ extension MusicPitch {
         self = .init(name: name, accidental: .natural, octave: octave)
     }
     
-    static let c: MusicPitch = .init(name: .c)
-    static let d: MusicPitch = .init(name: .d)
-    static let e: MusicPitch = .init(name: .e)
-    static let f: MusicPitch = .init(name: .f)
-    static let g: MusicPitch = .init(name: .g)
-    static let a: MusicPitch = .init(name: .a)
-    static let b: MusicPitch = .init(name: .b)
+    public static let c: MusicPitch = .init(name: .c)
+    public static let d: MusicPitch = .init(name: .d)
+    public static let e: MusicPitch = .init(name: .e)
+    public static let f: MusicPitch = .init(name: .f)
+    public static let g: MusicPitch = .init(name: .g)
+    public static let a: MusicPitch = .init(name: .a)
+    public static let b: MusicPitch = .init(name: .b)
     
-    func accidental(_ accidental: MusicAccidental) -> MusicPitch {
+    public func accidental(_ accidental: MusicAccidental) -> MusicPitch {
         var returnSelf = self
         returnSelf.accidental = accidental
         return returnSelf
     }
     
-    func octave(_ octave: Int) -> MusicPitch {
+    public func octave(_ octave: Int) -> MusicPitch {
         var returnSelf = self
         returnSelf.octave = octave
         return returnSelf
     }
     
-    func length(_ length: MusicRhythm) -> MusicNote {
+    public func length(_ length: MusicRhythm) -> MusicNote {
         MusicNote(pitch: self, rhythm: length)
     }
 }
