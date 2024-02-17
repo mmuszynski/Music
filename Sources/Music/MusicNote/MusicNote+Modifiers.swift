0391c6cab39a203cf9cb21e75997a99628a43718
Mike Muszynski <mmuszynski@gmail.com>
2024-02-17 10:40:37 -0500

    more convenience

--- Sources/Music/MusicNote/MusicNote+Modifiers.swift
+++ Sources/Music/MusicNote/MusicNote+Modifiers.swift
@@ -37,27 +37,59 @@ extension MusicPitch {
     }
     
     //A computed property to set the accidental of the pitch to a sharp
-    var sharp: Self {
+    public var sharp: Self {
         self.accidental(.sharp)
     }    
     
     //A computed property to set the accidental of the pitch to a natural
-    var natural: Self {
+    public var natural: Self {
         self.accidental(.natural)
     }
     
     //A computed property to set the accidental of the pitch to a flat
-    var flat: Self {
+    public var flat: Self {
         self.accidental(.flat)
     }
     
     //A computed property to set the accidental of the pitch to a double flat
-    var doubleFlat: Self {
+    public var doubleFlat: Self {
         self.accidental(.doubleFlat)
     }    
     
     //A computed property to set the accidental of the pitch to a double flat
-    var doubleSharp: Self {
+    public var doubleSharp: Self {
         self.accidental(.doubleSharp)
+    }
+}
+
+//Properties that make `MusicNote` objects out of `MusicPitch` objects
+extension MusicPitch {
+    
+    public var quarter: MusicNote {
+        self.note(with: .quarter)
+    }
+    
+    public var half: MusicNote {
+        self.note(with: .half)
+    }
+    
+    public var whole: MusicNote {
+        self.note(with: .whole)
+    }
+    
+    public var eighth: MusicNote {
+        self.note(with: .eighth)
+    }
+    
+    public var sixteenth: MusicNote {
+        self.note(with: .sixteenth)
+    }
+    
+    public var thirtysecond: MusicNote {
+        self.note(with: .thirtysecond)
+    }
+    
+    public var sixtyfourth: MusicNote {
+        self.note(with: .sixtyfourth)
     }
 }
